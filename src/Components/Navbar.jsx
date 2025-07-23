ec6c22bf331932b424f2650b8fdd3217f14d5362
David <yugiohgxd600@gmail.com>
2025-07-23 05:56:37 -0700

    add routes and 404.html

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="flex bg-black w-full h-[60px] text-white">
+      {/* Part 1 */}
+      <div className="flex gap-1 p-2.5">
+        <img src="logo.webp" alt="amazon-logo" className="w-24 cursor-pointer touch" />
+        <div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+
+      {/* Part 2 */}
+      <div></div>
+
+      {/* Part 3 */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
