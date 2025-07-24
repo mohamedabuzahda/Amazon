840779af7e3dc81a65253ab015ca1cc13b37137e
David <yugiohgxd600@gmail.com>
2025-07-24 07:58:39 -0700

    add navbar and banner

---
+++ src/Components/NavbarBanner.jsx
@@ -0,0 +1,44 @@
+import { IoMenuSharp } from "react-icons/io5";
+
+const list = [
+  "Amazon mini TV",
+  "Sell",
+  "Best Sellers",
+  "Today's Deals",
+  "Mobiles",
+  "Customer Service",
+  "Prime",
+  "Electronics",
+  "Fashion",
+  "New Releases",
+  "Home & Kitchen",
+  "Amazon pay",
+];
+
+const NavbarBanner = () => {
+  return (
+    <div className="bg-[#232f3e] text-white h-10 px-2 flex items-center gap-2 scroll-hide">
+      {/* زرار All */}
+      <div className="flex items-center gap-1 px-3 py-1 cursor-pointer touch border border-transparent hover:border-white rounded-[3px] shrink-0">
+        <IoMenuSharp className="text-xl" />
+        <p className="text-sm whitespace-nowrap">All</p>
+      </div>
+
+      {/* القائمة نفسها */}
+      <div
+        className="flex flex-row items-center gap-4 whitespace-nowrap scroll-smooth snap-x snap-mandatory scrollbar-hide"
+      >
+        {list.map((item, index) => (
+          <p
+            key={index}
+            className="text-sm cursor-pointer px-3 py-1 border border-transparent hover:border-white rounded-[3px] snap-start shrink-0"
+          >
+            {item}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NavbarBanner;
